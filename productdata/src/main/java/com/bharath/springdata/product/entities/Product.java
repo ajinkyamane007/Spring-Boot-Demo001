3b90ac15393a42a361c8202bbdf724a877fad71c
ajinkyamane007 <ajinkyamane3996@gmail.com>
2020-06-10 15:05:40 +0530

    "Minor change"

--- productdata/src/main/java/com/bharath/springdata/product/entities/Product.java
+++ productdata/src/main/java/com/bharath/springdata/product/entities/Product.java
@@ -4,7 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+//Use different keys
 @Entity
 @Table
 public class Product 
