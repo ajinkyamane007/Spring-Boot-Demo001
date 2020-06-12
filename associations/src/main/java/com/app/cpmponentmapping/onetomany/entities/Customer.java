f245a8c4fdff5b944fd9615a4b283ef60c1779d8
ajinkyamane007 <ajinkyamane3996@gmail.com>
2020-06-12 11:14:02 +0530

    Read Update added

--- associations/src/main/java/com/app/cpmponentmapping/onetomany/entities/Customer.java
+++ associations/src/main/java/com/app/cpmponentmapping/onetomany/entities/Customer.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -20,7 +21,7 @@ public class Customer
 	private String name;
 	
 	//It tells that primary key is customer  & owns/drive the mapping
-	@OneToMany(mappedBy = "customer",cascade = CascadeType.ALL)
+	@OneToMany(mappedBy = "customer",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
 	private Set<PhoneNumber>numbers;
 
 	public long getId() {
--- associations/src/test/java/com/app/cpmponentmapping/AssociationsApplicationTests.java
+++ associations/src/test/java/com/app/cpmponentmapping/AssociationsApplicationTests.java
@@ -1,10 +1,13 @@
 package com.app.cpmponentmapping;
 
 import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.app.cpmponentmapping.onetomany.entities.Customer;
 import com.app.cpmponentmapping.onetomany.entities.PhoneNumber;
@@ -23,7 +26,7 @@ class AssociationsApplicationTests
 	@Test
 	public void testCreateCustomer() {
 		Customer cus=new Customer();
-		cus.setName("Sumit");
+		cus.setName("Abhijeet");
 	//	HashSet<PhoneNumber>numbers=new HashSet<PhoneNumber>();
 		
 		PhoneNumber ph1=new PhoneNumber();
@@ -34,17 +37,45 @@ class AssociationsApplicationTests
 		
 		PhoneNumber ph2=new PhoneNumber();
 		ph2.setNumber("7743674445");
-		ph2.setType("cell");
+		ph2.setType("home");
 	//	ph2.setCustomer(cus);
 	//	numbers.add(ph2);
 		
 	//	cus.setNumbers(numbers);
 		
 		cus.addPhoneNumber(ph1);
-		cus.addPhoneNumber(ph1);
+		cus.addPhoneNumber(ph2);
 		
 		repository.save(cus);
 		
 	} 
+	
+	@Test
+	@Transactional
+	public void testLoadCustomer() {
+		Optional<Customer> cus =repository.findById(5L);
+		if (cus.isPresent())
+		{
+			Customer customer=cus.get();
+			System.out.println(customer.getName());
+			
+			Set<PhoneNumber>numbers=customer.getNumbers();
+			numbers.forEach(number->System.out.println(number.getNumber()));
+		}	
+	}
+	
+	@Test
+	@Transactional
+	public void testUpdateCustomer() {
+		Optional<Customer> cus =repository.findById(5L);
+		if (cus.isPresent())
+		{
+			Customer customer=cus.get();
+			customer.setName("Abhijeet Mane");
+			
+			Set<PhoneNumber>numbers=customer.getNumbers();
+			numbers.forEach(number->number.setType("cell"));
+		}	
+	}
 
 }
