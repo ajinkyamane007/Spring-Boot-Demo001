42111504477b7dd9d784c93c7039b5bbdee1a4f6
ajinkyamane007 <ajinkyamane3996@gmail.com>
2020-06-20 17:53:54 +0530

    Transaction management

--- transactionalmanagement/src/main/java/com/app/transactionalmanagement/services/BanckAccountServiceImpl.java
+++ transactionalmanagement/src/main/java/com/app/transactionalmanagement/services/BanckAccountServiceImpl.java
@@ -16,30 +16,35 @@ public class BanckAccountServiceImpl implements BanckAccountService {
 
 	@Autowired
 	BanckAccountRepository repository;
-	
+
 	@Override
 	@Transactional  // To rollback transaction when out of two at least one transaction if fail
-	public void transfer(int amount) 
-	{
-		Optional<BanckAccount> acccount1=repository.findById(1);
-		if (acccount1.isPresent())
-		{ 
+	public void transfer(int amount) {
+		Optional<BanckAccount> acccount1 = repository.findById(1);
+		if (acccount1.isPresent()) {
 			BanckAccount acc1 = acccount1.get();
-			acc1.setBal( acc1.getBal() - amount );
+			acc1.setBal(acc1.getBal() - amount);
 			repository.save(acc1);
 		}
 		
-//		if(true) {
-//			throw new RuntimeErrorException(null);
-//		}
+		//if(true) {
+		//throw new RuntimeErrorException(null);
+		//}
 		
-		Optional<BanckAccount> acccount2=repository.findById(2);
-		if (acccount2.isPresent())
-		{
+		Optional<BanckAccount> acccount2 = repository.findById(2);
+		if (acccount2.isPresent()) {
 			BanckAccount acc2 = acccount2.get();
-			acc2.setBal( acc2.getBal() + amount);
+			acc2.setBal(acc2.getBal() + amount);
 			repository.save(acc2);
 		}
 	}
 
 }
+
+//Optional<BanckAccount> acccount1=repository.findById(1);
+//acccount1.get().setBal( acccount1.get().getBal() - amount );
+//repository.save(acccount1.get());
+//
+//Optional<BanckAccount> acccount2=repository.findById(1);
+//acccount2.get().setBal( acccount2.get().getBal() + amount );
+//repository.save(acccount2.get());
--- transactionalmanagement/src/test/java/com/app/transactionalmanagement/TransactionalmanagementApplicationTests.java
+++ transactionalmanagement/src/test/java/com/app/transactionalmanagement/TransactionalmanagementApplicationTests.java
@@ -3,6 +3,8 @@ package com.app.transactionalmanagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.app.transactionalmanagement.services.BanckAccountService;
 
@@ -13,6 +15,8 @@ class TransactionalmanagementApplicationTests {
 	BanckAccountService service;
 	
 	@Test
+//	@Transactional
+//	@Rollback(false)
 	void testTransfer() {
 		service.transfer(500);
 	}
